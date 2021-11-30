472108bd50a87529887846111914ded8461a3852
unknown <santiagobalino@hotmail.com>
2021-11-30 08:27:18 -0300

    First commit

---
+++ src/Components/GifExpertApp.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    'Sasuke',
+  ]);
+
+  return (
+    <div className="container-card">
+      <h2>Gif Expert App</h2>
+      <AddCategory setCategories={setCategories} />
+      <ol>
+        {categories.map((category) => <GifGrid 
+        key={ category }
+        category={ category }/>)}
+      </ol>
+    </div>
+  );
+};
---
+++ src/Components/GifGrid.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItems } from './GifGridItems';
+
+export const GifGrid = ({ category }) => {
+  const { data: images, loading } = useFetchGifs(category);
+
+  return (
+    <>
+      <h3 className='category animate__animated animate__fadeIn'>{category}</h3>
+      <div className={'card-grid'}>
+        {loading && <p className='animate__animated animate__flash'>Loading</p>}
+        {images.map((img) => (
+          <GifGridItems key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
